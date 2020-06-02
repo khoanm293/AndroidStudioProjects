3a3371d0e4f3eaeb490a3953bfeb971a32c6baa1
Nguyen Khoa <khoauet293@gmail.com>
2020-06-02 21:41:29 +0700

    [Google][Map] Demo use Google map API - Register Google map API - Search special location in map

---
+++ DemoGoogleMap/app/src/main/java/com/example/demogooglemap/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.demogooglemap;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map_fragment);
+        mapFragment.getMapAsync(this);
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.mMap = googleMap;
+        LatLng vn = new LatLng(14.0583, 108.2772);
+        this.mMap.addMarker(new MarkerOptions().position(vn).title("Viet Nam"));
+        this.mMap.moveCamera(CameraUpdateFactory.newLatLng(vn));
+        this.mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                MarkerOptions markerOptions = new MarkerOptions();
+                markerOptions.position(latLng);
+                markerOptions.title(latLng.latitude + " : " + latLng.longitude);
+                mMap.clear();
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+                mMap.addMarker(markerOptions);
+            }
+        });
+    }
+}
